785ba231b5ddd1af9b937f28c49b63f0adfa2d45
M Durga Subendra <you@example.com>
2022-06-16 12:52:54 +0530

    added search bar and fetched data from api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>This is Home</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onClickButton = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(res => res.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onEnter = ev => {
+    if (ev.keyCode === 13) {
+      onClickButton();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <div>
+        <input
+          type="text"
+          onChange={onInputChange}
+          onKeyDown={onEnter}
+          value={input}
+        />
+        <button type="button" onClick={onClickButton}>
+          Search
+        </button>
+      </div>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
