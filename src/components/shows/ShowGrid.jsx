cb86ad94fb2da8a5f9380ac614f4a04e7b674b33
M Durga Subendra <you@example.com>
2022-06-20 20:32:23 +0530

    added starred functionality

--- src/components/shows/ShowGrid.jsx
+++ src/components/shows/ShowGrid.jsx
@@ -3,11 +3,23 @@ import ShowCard from './ShowCard';
 
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
 import { FlexGrid } from '../styled';
+import { useShows } from '../../misc/custom-hooks';
 
 const ShowGrid = ({ shows }) => {
+  const [starredShows, dispatchStarred] = useShows();
+
   return (
     <FlexGrid>
       {shows.map(({ show }) => {
+        const isStarred = starredShows.includes(show.id);
+
+        const onStarClick = () => {
+          if (isStarred) {
+            dispatchStarred({ type: 'REMOVE', showId: show.id });
+          } else {
+            dispatchStarred({ type: 'ADD', showId: show.id });
+          }
+        };
         return (
           <ShowCard
             key={show.id}
@@ -15,6 +27,8 @@ const ShowGrid = ({ shows }) => {
             name={show.name}
             image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
             summary={show.summary}
+            onStarClick={onStarClick}
+            isStarred = {isStarred}
           />
         );
       })}
