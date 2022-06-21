2379ee7a687099c20feb07ebd9ed1dd05742e738
M Durga Subendra <you@example.com>
2022-06-21 10:27:48 +0530

    created more custom hooks

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,62 +1,18 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useEffect, useReducer } from 'react';
+
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import Cast from '../components/shows/Cast';
-
 import Details from '../components/shows/Details';
 import Seasons from '../components/shows/Seasons';
-
 import ShowMainData from '../components/shows/ShowMainData';
-import { apiUrl } from '../misc/config';
+import { useShow } from '../misc/custom-hooks';
 import { InfoBlock, ShowPageWrapper } from './Show.style';
-
-const reducer = (prevState, action) => {
-  switch (action.type) {
-    case 'FETCH_SUCCESS': {
-      return { isLoading: false, show: action.show, error: null };
-    }
-
-    case 'FETCH_FAILED': {
-      return { ...prevState, isLoading: false, error: action.error };
-    }
-    default:
-      return prevState;
-  }
-};
-
-const initialState = {
-  show: null,
-  isLoading: true,
-  error: null,
-};
 
 const Show = () => {
   const { id } = useParams();
 
-  const [{ show, isLoading, error }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
-
-  useEffect(() => {
-    let isMounted = true;
-
-    apiUrl(`/shows/${id}?embed[]=seasons&embed[]=cast`)
-      .then(results => {
-        if (isMounted) {
-          dispatch({ type: 'FETCH_SUCCESS', show: results });
-        }
-      })
-      .catch(err => {
-        if (isMounted) {
-          dispatch({ type: 'FETCH_FAILED', error: err.message });
-        }
-      });
-
-    return () => {
-      isMounted = false;
-    };
-  }, [id]);
+  const { show, isLoading, error } = useShow(id);
 
   console.log('show', show);
 
